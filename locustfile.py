fc19c92c6806c3052830a8c3b4c7ae02386868f6
etaeaii <150453896+etaeaii@users.noreply.github.com>
2026-02-27 10:44:40 +0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,17 @@
 from locust import HttpUser, task, between
 
-class WebsiteUser(HttpUser):
-    wait_time = between(1, 5)
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    def on_start(self):
+        self.client.post("/login", json={"username":"foo", "password":"bar"})
 
     @task
-    def my_task(self):
-        self.client.get("/")
+    def hello_world(self):
+        self.client.get("/hello")
+        self.client.get("/world")
+
+    @task(3)
+    def view_item(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
